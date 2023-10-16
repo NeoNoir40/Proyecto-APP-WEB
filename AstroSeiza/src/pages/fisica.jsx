5004fe4459deae99462e7a1d8d5bf913d8dcd4bc
Jacob0418 <cjosecoronado97@gmail.com>
2023-10-15 22:01:20 -0500

    ola mundo

--- AstroSeiza/src/pages/fisica.jsx
+++ AstroSeiza/src/pages/fisica.jsx
@@ -1,19 +1,10 @@
-import React from "react";
-import iconfisica from "../assets/lottieIcons/fisica.json";
-import Lottie from "lottie-react";
-import Navbar from "../components/Navbar";
-
-function Physical() {
+export default function Physical() {
     <>
-        <div>
-            <h1 className="text-white">HOLLAAAA</h1>
-        </div>
+        <h1 className="text-white">HOLLAAAA</h1>
         <div className="text-white">
-            <p>La Física es la ciencia natural que estudia, 
-            <br/>mediante leyes fundamentales, la energía, la materia, el tiempo y el espacio,
-            <br/>es decir, el universo mismo.</p>
+            <p>La Física es la ciencia natural que estudia,
+                <br />mediante leyes fundamentales, la energía, la materia, el tiempo y el espacio,
+                <br />es decir, el universo mismo.</p>
         </div>
     </>
 }
-
-export default Physical;
