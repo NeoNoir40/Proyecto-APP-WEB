4ee47f7ea5f76552e44ed1bbf77ae1e35895efc3
NeoNoir40 <neroxartz@hotmail.com>
2023-10-19 08:56:44 -0500

    Agregacion de maquetado de  modulo de fase de luna

--- AstroSeiza/src/pages/Astronomia.jsx
+++ AstroSeiza/src/pages/Astronomia.jsx
@@ -18,6 +18,7 @@ export default function Astronomia() {
   return (
     <>
       <div className="grid p-20">
+        {/* Modulo de incio */}
         <div className="flex flex-colum justify-start ">
           <div className="flex">
             <div className="flex">
@@ -75,6 +76,7 @@ export default function Astronomia() {
             <h1 className="text-black">Pinteresst API</h1>
           </div>
         </div>
+        {/* Modulo de tipo de astros */}
         <div>
           <div className="flex flex-colum justify-center">
             <div className="grid">
@@ -129,10 +131,47 @@ export default function Astronomia() {
                 </div>
               </div>
               <div className="flex flex-colum justify-center m-auto p-[100px]">
-                <div className={apiFondo ? "fondo-blanco" : ""}>
-                  <h1 className="text-black text-center">Informacion del astro</h1>
-                </div>
+                <div className={apiFondo ? "fondo-blanco" : ""}></div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-colum  justify-start bg-black">
+          <div className="flex text-white font-bold">
+            {/* Modulo de la luna */}
+            <div className="bg-red-900">
+              <div className="bg-blue-900">
+                <h1>Fases de la luna</h1>
+              </div>
+              <div className="bg-green-900">
+                <h1>Moon Icon</h1>
+              </div>
+              <div className="bg-pink-400">
+                <p>
+                  Laika fue el primer ser vivo en orbitar la Tierra. Fue una
+                  perra enviada al espacio por la Unión Soviética en 1957, pero
+                  trágicamente no sobrevivió al viaje.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Modulo de la luna */}
+          <div className="bg-purple-400">
+            <div className="bg-purple-700">
+              <h1>Laika</h1>
+            </div>
+            <div className="bg-red-500">
+              <h1>Container</h1>
+            </div>
+            <div className="bg-green-400">
+              <p>
+                Las fases de la luna son diferentes aspectos visibles de la Luna
+                a medida que orbita alrededor de la Tierra. Van desde la Luna
+                nueva, cuando no se ve, hasta la Luna llena, cuando se ve
+                completamente iluminada, pasando por el cuarto creciente y el
+                cuarto menguante. Estas fases cambian a lo largo de un ciclo
+                lunar de aproximadamente 29.5 días. Existen 8 fases de la luna
+              </p>
             </div>
           </div>
         </div>
