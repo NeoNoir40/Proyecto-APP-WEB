251003b3b19a4ce69550542e35df6ee1bbb17c4b
Jacob0418 <cjosecoronado97@gmail.com>
2023-10-21 19:53:05 -0500

    new change

--- AstroSeiza/src/pages/fisica.jsx
+++ AstroSeiza/src/pages/fisica.jsx
@@ -2,6 +2,7 @@ import Lottie from "lottie-react";
 import fis from '../assets/lottieIcons/animation_lntab4at.json';
 import sec from '../assets/lottieIcons/icons8-casa.json'
 import thre from '../assets/lottieIcons/clima.json'
+import img from '../assets/img/arrow.png'
 
 export default function Physical() {
     return (
@@ -31,15 +32,36 @@ export default function Physical() {
                 />
             </div>
             <div className="text-white flex flex-row items-center justify-center mr-48">
-            <Lottie
+                <Lottie
                     animationData={thre}
                     className="h-[180px] w-[180px] mr-4"
                 />
                 <div className=" mr-14 h-auto">
                     <h1 className="h-9 text-xl font-bold text-start">CLIMA</h1>
-                    <p className="text-start">Es el estado en que se encuentra la atmósfera en ese momento 
-                    <br/>en cortos períodos, generalmente horas o pocos días. Si se asoma a la 
-                    <br/>ventana, se podrá observar cómo está el tiempo atmosférico.</p>
+                    <p className="text-start">Es el estado en que se encuentra la atmósfera en ese momento
+                        <br />en cortos períodos, generalmente horas o pocos días. Si se asoma a la
+                        <br />ventana, se podrá observar cómo está el tiempo atmosférico.</p>
+                    <p>Consultar clima: </p>
+                </div>
+            </div>
+            <div className=" flex justify-center items-center h-auto flex-col">
+                <div className=" bg-yellow-200 w-72 h-10 rounded-[30px_30px_30px_30px] border-4 border-yellow-600">
+                    <p className="ml-5 mt-1 text-left font-medium">Clima hoy</p>
+                </div>
+                <div className=" bg-yellow-200 w-72 h-10 rounded-[30px_30px_30px_30px] border-4 border-yellow-600 my-1">
+                    <p className="ml-5 mt-1 text-left font-medium">Incendios Forestales</p>
+                </div>
+                <div className=" bg-yellow-200 w-72 h-10 rounded-[30px_30px_30px_30px] border-4 border-yellow-600 my-0">
+                    <p className="ml-5 mt-1 text-left font-medium">Ciudades Vecinas</p>
+                </div>
+                <div className=" bg-yellow-200 w-72 h-10 rounded-[30px_30px_30px_30px] border-4 border-yellow-600 my-1">
+                    <p className="ml-5 mt-1 text-left font-medium">Ciclones</p>
+                </div>
+                <div className=" bg-yellow-200 w-72 h-10 rounded-[30px_30px_30px_30px] border-4 border-yellow-600 my-0">
+                    <p className="ml-5 mt-1 text-left font-medium">Ciclones del Año</p>
+                </div>
+                <div className=" bg-yellow-200 w-72 h-10 rounded-[30px_30px_30px_30px] border-4 border-yellow-600 my-1">
+                    <p className="ml-5 mt-1 text-left font-medium">Por Localidad</p>
                 </div>
             </div>
         </>
