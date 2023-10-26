75a1870475f4c9d551f49f22d2eba9f0a233fee6
NeoNoir40 <neroxartz@hotmail.com>
2023-10-26 04:54:18 -0500

    Integracion de api de la nasa para imagen/video diario

--- AstroSeiza/src/components/musicPlayerAstronomi.jsx
+++ AstroSeiza/src/components/musicPlayerAstronomi.jsx
@@ -1,21 +1,10 @@
-import Player from "@madzadev/audio-player";
 import music from "../assets/audio/HaydenFolkerAdrift.mp3";
 export default function AstroSong() {
-//   const tracks = [
-//     {
-//       url: music,
-//       title: "Madza - Chords of Life",
-//       tags: ["BackgroundSound"],
-//     },
-//   ];
   return (
     <div className="justify-end  flex items-end mt-10">
-      {/* <div className="p-10 h-[200px]  w-[500px] ">
-        <Player trackList={tracks} />
-      </div> */}
-    <audio controls autoplay loop>
-        <source src={music} type="audio/mpeg"/>
-    </audio>
+      <audio controls autoplay loop>
+        <source src={music} type="audio/mpeg" />
+      </audio>
     </div>
   );
 }
